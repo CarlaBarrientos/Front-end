8b661fdac88cb641aea56d26e2f39e9703fdc62d
CarlaBarrientos <carla.andrea.cbm@gmail.com>
2022-05-30 10:31:34 -0400

    Homework: production file updated.

--- Pokemon/webpack.prod.js
+++ Pokemon/webpack.prod.js
@@ -2,6 +2,7 @@ const path = require("path");
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = merge(common, {
     mode: "production",
@@ -9,11 +10,12 @@ module.exports = merge(common, {
         filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, 'build')
     },
+    devtool: 'source-map',
     module: {
         rules: [
             {
                 test: /\.css$/i,
-                use: ['style-loader', 'css-loader']
+                use: [MiniCssExtractPlugin.loader, 'style-loader', 'css-loader']
             },
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
@@ -28,5 +30,17 @@ module.exports = merge(common, {
     plugins: [new HtmlWebpackPlugin({
         title: 'Production',
         template: path.resolve(__dirname, './src/index.html')
-    })]
+    })],
+    optimization: {
+        runtimeChunk: 'single',
+        splitChunks: {
+            minSize: 0,
+            cacheGroups: {
+                defaultVendors: {
+                    test: /[\\/]node_modules[\\/]/,
+                    chunks: 'all'
+                }
+            }
+        }
+    }
 });
