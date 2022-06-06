12579a0c5305622a15e2f5a8bee11bca8e180c3d
CarlaBarrientos <carla.andrea.cbm@gmail.com>
2022-06-06 12:37:32 -0400

    Homework: adding decorator to check PP.

--- Decorators/pokemon-decorator.ts
+++ Decorators/pokemon-decorator.ts
@@ -1,38 +1,46 @@
-const checkPP = (ppAvailable: number) => (
-    target: Object,
-    prop: string,
-    descriptor: PropertyDescriptor) => {
-      const value = descriptor.value;
-      descriptor.value = function() {
-        if(ppAvailable <= 0) value.apply(this);
-        else console.log('You can not attack to other Pokemons')
-      }
-  }
-  type Move = {
-      name: string,
-      power: number
-  };
+const checkPP = () => (
+	target: Object,
+	propertyKey: string,
+	descriptor: PropertyDescriptor
+  ) => {
+	const originalMethod = descriptor.value;
   
-  class Pokemon {
-    name: string;
-    ppAvailable = 1;
-    constructor(name: string, ppAvailable: number) {
-      this.name = name;
-      this.ppAvailable = ppAvailable;
-    }
-    
-    @checkPP(0)
-    figth(move: Move) {
-      console.log(`${this.name} used ${move?.name}!`);
-      this.ppAvailable -= 1;
-    }
+	descriptor.value = function (...args: any) {
+		const pokemon = this as Pokemon;
+	  if (pokemon.ppAvailable > 0) {
+		originalMethod.apply(this, args);
+	  } else {
+		console.log('You can not attack to other Pokemons');
+	  }
+	};
   
-    calculateDamage(move: any) {
-      return move.power;
-    }
-  }
+	return descriptor;
+  }; 
+type Move = {
+	name: string,
+	power: number
+};
   
-  const thunderbolt: Move = {name: 'thunderbolt', power: 90};
-  const pikachu = new Pokemon('pikachu', 1);
-  pikachu.figth(thunderbolt);
-  pikachu.figth(thunderbolt);
+class Pokemon {
+	name: string;
+	ppAvailable = 1;
+	constructor(name: string, ppAvailable: number) {
+		this.name = name;
+		this.ppAvailable = ppAvailable;
+	}
+
+	@checkPP()
+	figth({ move }: { move: Move; }): void {
+		console.log(`${this.name} used ${move?.name}!`);
+		this.ppAvailable -= 1;
+	}
+
+	calculateDamage(move: any) {
+		return move.power;
+	}
+}
+
+const thunderbolt: Move = {name: 'thunderbolt', power: 90};
+const pikachu = new Pokemon('pikachu', 1);
+pikachu.figth({ move: thunderbolt });
+pikachu.figth({ move: thunderbolt });
