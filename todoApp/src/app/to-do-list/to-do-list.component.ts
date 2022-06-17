84704d2576433f806df826c4a30a430af92904d4
CarlaBarrientos <carla.andrea.cbm@gmail.com>
2022-06-17 13:21:49 -0400

    Homework: todoApp completed.

---
+++ todoApp/src/app/to-do-list/to-do-list.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+
+type Task = {
+    descripcion: string;
+    hecho: boolean;
+}
+@Component({
+    selector: 'app-to-do-list',
+    templateUrl: './to-do-list.component.html',
+    styleUrls: ['./to-do-list.component.scss']
+})
+export class TodoListComponent implements OnInit {
+    nuevaTarea: string = '';
+    listaTareas: Task[] = [];
+    done: boolean = false;
+
+    constructor() { }
+    ngOnInit(): void {
+    }
+
+    crearTarea() {
+        this.listaTareas.push({
+            descripcion: this.nuevaTarea,
+            hecho: false        
+        });
+        this.nuevaTarea = '';
+    }
+
+    eliminarTarea(index: number) {
+        this.listaTareas.splice(index, 1);
+    }
+
+    marcarTarea(index: number) {
+        this.listaTareas[index].hecho = !this.listaTareas[index].hecho;
+    }
+}
