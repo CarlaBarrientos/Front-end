66e8009eaccace9d1263e245c81930a1cc7a0ff1
Carla.Barrientos <Carla.Barrientos@jala-foundation.org>
2022-07-21 15:12:10 -0400

    PokemonProject: fixing styles.

--- PokeapiAngular/src/app/pokemons/pokemon/profile/components/stats-card.component.ts
+++ PokeapiAngular/src/app/pokemons/pokemon/profile/components/stats-card.component.ts
@@ -1,3 +1,4 @@
+import { getLocaleDateFormat } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
 import { EChartsOption } from 'echarts';
 
@@ -11,7 +12,7 @@ export class StatsCardComponent implements OnInit {
     @Input()
     pokemonStats!: any;
     categories: string[] = [];
-    values: number[] = [];
+    values: { value: number, itemStyle: { color: string } }[] = [];
 
     chartOption: EChartsOption = {}
 
@@ -25,7 +26,12 @@ export class StatsCardComponent implements OnInit {
     fillChartWithStats() {
         this.pokemonStats.forEach((stat: { base_stat: number, stat: { name: string } }) => {
             this.categories.push(stat.stat.name);
-            this.values.push(stat.base_stat);
+            this.values.push({
+                value: stat.base_stat,
+                itemStyle: {
+                    color: this.getColor(stat.base_stat)
+                }
+            });
         });
         this.chartOption = {
             xAxis: {
@@ -43,6 +49,18 @@ export class StatsCardComponent implements OnInit {
                 },
             ],
         };
+    }
+
+    getColor(value: number) {
+        if(value > 0 && value < 25) {
+            return '#CD6155';
+        } else if(value > 24 && value < 50) {
+            return '#F5B041';
+        } else if(value > 49 && value < 75) {
+            return '#F4D03F';
+        } else {
+            return '#58D68D'
+        }
     }
 
 }
